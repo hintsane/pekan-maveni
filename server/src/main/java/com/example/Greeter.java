65fb832083d8349aceb13ae4b6c2618bb729731f
pekka <pekka>
2017-02-21 12:00:02 +0200

    KOMMENTTILOISSII

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,11 @@ public class Greeter {
 
   }
 
+  /**
+  * @param PARAMEETRI
+  * @return RETUKAINEN
+  */
+
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
